3fb84b26287cda15e032add2834de995f5dc40d2
K-F Balázs <150327324+WotBALU@users.noreply.github.com>
2025-11-05 09:24:47 +0100

    Add files via upload

---
+++ Orszag.cs
@@ -0,0 +1,8 @@
+﻿namespace Euroskills2018.Models
+{
+    public class Orszag
+    {
+        public string Id { get; set; }
+        public string OrszagNev { get; set; }
+    }
+}
---
+++ Szakma.cs
@@ -0,0 +1,8 @@
+﻿namespace Euroskills2018.Models
+{
+    public class Szakma
+    {
+        public string Id { get; set; }
+        public string SzakmaNev { get; set; }
+    }
+}

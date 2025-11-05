35b906443bc7c4b1a1bd81c4e4e7d4c9b1867a3f
K-F Balázs <150327324+WotBALU@users.noreply.github.com>
2025-11-05 09:25:44 +0100

    Add files via upload

---
+++ EuroskillsContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Euroskills2018.Models;
+
+namespace Euroskills2018.Data
+{
+    public class EuroskillsContext : DbContext
+    {
+        public EuroskillsContext(DbContextOptions<EuroskillsContext> options)
+            : base(options) { }
+
+        public DbSet<Orszag> Orszagok { get; set; }
+        public DbSet<Szakma> Szakmak { get; set; }
+        public DbSet<Versenyzo> Versenyzok { get; set; }
+    }
+}
